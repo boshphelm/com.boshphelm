76d4b97e3ba875719688d05b3f5955b83c57511c
mrakbulut <50949239+mrakbulut@users.noreply.github.com>
2024-07-17 15:12:48 +0300

    fix: Test

--- Tests/Runtime/TestSC.cs
+++ Tests/Runtime/TestSC.cs
@@ -6,16 +6,6 @@ namespace boshphelm.Tests
 {
     public class TestSC : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
         
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
