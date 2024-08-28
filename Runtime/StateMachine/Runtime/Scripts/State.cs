7ebb6c0fd19cd2f7d9600de8136fa1a08c71e514
mrakbulut <50949239+mrakbulut@users.noreply.github.com>
2024-08-28 18:11:57 +0300

    fix: State History removed

--- Runtime/StateMachine/Runtime/Scripts/State.cs
+++ Runtime/StateMachine/Runtime/Scripts/State.cs
@@ -5,6 +5,6 @@ namespace Boshphelm.StateMachines
         public abstract void Enter();
         public abstract void Exit();
         public abstract void Tick();
-        public abstract string GetName();
+        //public abstract string GetName();
     }
 }
--- Runtime/StateMachine/Runtime/Scripts/StateMachine.cs
+++ Runtime/StateMachine/Runtime/Scripts/StateMachine.cs
@@ -7,12 +7,12 @@ namespace Boshphelm.StateMachines
     public abstract class StateMachine : MonoBehaviour
     {
         private State _currentState;
-        private List<string> _stateHistory;
-        protected virtual void Awake()
+        //private List<string> _stateHistory;
+        /* protected virtual void Awake()
         {
-            _stateHistory = new List<string>();
+            //_stateHistory = new List<string>();
 
-        }
+        } */
 
         public void SwitchState(State newState)
         {
@@ -21,17 +21,17 @@ namespace Boshphelm.StateMachines
             _currentState = newState;
             _currentState?.Enter();
 
-            _stateHistory.Add(_currentState?.GetName());
+            //  _stateHistory.Add(_currentState?.GetName());
         }
 
         private void Update()
         {
             _currentState?.Tick();
         }
-        public List<string> GetStateHistory()
+        /* public List<string> GetStateHistory()
         {
             return new List<string>(_stateHistory);
-        }
+        } */
 
     }
 }
