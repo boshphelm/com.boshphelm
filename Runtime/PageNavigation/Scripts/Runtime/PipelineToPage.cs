0d86b0c9d36d7ea7765a62ae2cb2d422c0c443f1
mrakbulut <50949239+mrakbulut@users.noreply.github.com>
2024-12-16 20:24:48 +0300

    - Weapon, Hero and Upgrade

---
+++ Runtime/PageNavigation/Scripts/Runtime/PipelineToPage.cs
@@ -0,0 +1,29 @@
+using Boshphelm.Pages;
+
+namespace Boshphelm.PageNavigation
+{
+    public class PipelineToPage : Page
+    {
+        private Page _toPage;
+
+        public void SetPage(Page page)
+        {
+            if (_toPage != null) return;
+
+            _toPage = page;
+        }
+
+        public override void Open()
+        {
+            if (_toPage == null) return;
+
+            _toPage.Open();
+        }
+        public override void Close()
+        {
+            if (_toPage == null) return;
+
+            _toPage.Close();
+        }
+    }
+}
